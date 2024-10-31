a96b04e474adec49de612297d43a55ee74fd952a
MennaEbied <mennahesham253@gmail.com>
2024-10-31 16:47:40 +0300

    Add theme file for colors

---
+++ theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+    colorBlue:"#aacbe2",
+    colorWhite:"#fff",
+    colorDrakBlue:"#7ba6d6",
+    colorYellow:"#FFC857",
+    colorVanilla:"#FAF2A1",
+    colorBlack:"#000",
+    color4:"#FAC05E",
+    color5:"#528ab4"
+}

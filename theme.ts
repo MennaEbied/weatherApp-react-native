b32f98671c0dfb574c44ad1fef6e348274170c40
MennaEbied <mennahesham253@gmail.com>
2024-10-31 23:57:54 +0200

    Add colors

--- theme.ts
+++ theme.ts
@@ -2,9 +2,8 @@ export const theme = {
     colorBlue:"#aacbe2",
     colorWhite:"#fff",
     colorDrakBlue:"#7ba6d6",
-    colorYellow:"#FFC857",
-    colorVanilla:"#FAF2A1",
     colorBlack:"#000",
-    color4:"#FAC05E",
-    color5:"#528ab4"
+    colorYellow:"#F1B42F",
+    colorBrightYellow:"#f6cf79",
+    colorGrey:"#5555",
 }

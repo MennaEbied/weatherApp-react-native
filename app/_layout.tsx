dc55915bb2424d5a26b316faecb81bab4525260b
MennaEbied <mennahesham253@gmail.com>
2024-11-01 00:08:52 +0200

    Add weather screen to Stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,11 +3,21 @@ import { Stack } from "expo-router";
 export default function Layout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{
-         title: "Home" ,
-         headerShown:false
-         
-         }} />
+      <Stack.Screen 
+        name="index" 
+        options={{
+          title: "Home" ,
+          headerShown:false
+            }} 
+        />
+        <Stack.Screen 
+          name="weather" 
+          options={{
+            title:'weather',
+            
+
+          }} 
+        />
     </Stack>
   );
 }

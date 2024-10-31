dfabe222c0be437f1cd9168e0a0f9f6aa8041f1c
MennaEbied <mennahesham253@gmail.com>
2024-10-31 16:51:12 +0300

    Modified home screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,66 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View ,TextInput } from "react-native";
+import React , {useState} from "react";
+import {useRouter} from "expo-router";
+import { LogoImage } from "../components/logoImage";
+import { theme } from "../theme";
+import { SearchButton } from "../components/searchButton";
+import { SearchIcon } from "../components/searchIcon";
+import { IconImage } from "../components/icon";
+import { ThunderIcon } from "../components/thunder";
 
-export default function App() {
+
+
+export default function HomeScreen() {
+
+  const [city,setCity] = useState("");
+  const router = useRouter();
+
+  const handleSearch = ()=>{
+    router.push(`/weather?city=${city}`);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.search}>
+        <SearchIcon/>
+        <TextInput
+          placeholder="Enter a city" 
+          value={city}
+          onChangeText={setCity} 
+        />
+      </View>
+      <SearchButton title="Get Weather" onPress={handleSearch}/>
+        <IconImage/>
+        <LogoImage/>
+        <ThunderIcon/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+     backgroundColor:theme.colorBlue,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start",
+    paddingTop:70,
+    padding:16,
   },
+  search:{
+    flexDirection:"row",
+    alignItems:"center",
+    margin:10,
+    height:40,
+    borderWidth:1,
+    borderRadius:25,
+    width:"100%",
+    paddingLeft:12,
+    backgroundColor:theme.colorWhite,
+    borderColor:theme.colorBlack,
+    
+  },
+
 });
